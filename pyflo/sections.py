0b4b7cf09f314702a6f083cbb55ac5f9925c7c5f
Ben Miller <benjiyamin@gmail.com>
2017-06-20 23:23:12 -0400

    Simplified irregular section methods

--- pyflo/sections.py
+++ pyflo/sections.py
@@ -5,8 +5,8 @@
 
 """
 
-import collections
 import math
+from typing import List, Tuple
 
 
 class Section(object):
@@ -185,7 +185,7 @@ class Trapezoid(Section):
         """Get the wet perimeter of flow, given a depth from the invert.
 
         Args:
-            depth (float): Depth, in :math:`feet`
+            depth (float): Depth, in :math:`feet`.
 
         Returns:
             float: Wet perimeter, in :math:`feet`.
@@ -212,74 +212,62 @@ class Irregular(Section):
         super(Irregular, self).__init__(count, **kwargs)
         self.points = points
 
-    def get_new_vertices(self, depth):
-        """Get the new vertices of the cross section including
-            the intersection of ground line with the water surface, given a depth from the
-            lowest point.
+    def flow_vertices(self, depth):
+        """Get the new vertices of the cross section including the intersection of ground line with
+            the water surface, given a depth from the lowest point.
 
         Args:
-            depth (float): Depth, in :math:`feet`
+            depth (float): Depth, in :math:`feet`.
 
         Returns:
-            list of tuples (float) in (x, y) format
+            List[Tuple[float, float]]: The updated vertices (points).
 
         """
+        left = 0                                    # Water surface intersection at left
+        right = 0                                   # Water surface intersection at right
+        points = []
+        elev_water = self.elev_lowest + depth
 
-        left = 0                # Water surface intersection at left
-        right = 0               # Water surface intersection at right
-        new_points = []
-        water_elevation = self.get_lowest_elev + depth
+        for i, pt in enumerate(self.points):
+            x2, y2 = pt
+            pt_prev = self.points[i - 1]
+            x1, y1 = pt_prev
 
-        for index in range(len(self.points)):
-            x, y = self.points[index]
+            if left == 0 and y2 < elev_water:
+                left += 1
+                x3 = (elev_water-y1) * (x2-x1) / (y2-y1) + x1
+                points.append((x3, elev_water))
 
-            # Look for the first point of intersection
-            if left == 0:
-                if y < water_elevation:
-                    left += 1
-                    x1 = self.points[index-1][0]
-                    y1 = self.points[index-1][1]
-                    x2 = self.points[index][0]
-                    y2 = self.points[index][1]
-                    x3 = (water_elevation - y1) * (x2 - x1) / (y2 - y1) + x1
-                    new_points.append((x3, water_elevation))
-
-            if right == 0:
-                if left == 1:
-                    if y > water_elevation:
-                        right += 1
-                        x1 = self.points[index-1][0]
-                        y1 = self.points[index-1][1]
-                        x2 = self.points[index][0]
-                        y2 = self.points[index][1]
-                        x3 = (water_elevation - y1) * (x2 - x1) / (y2 - y1) + x1
-                        new_points.append((x3, water_elevation))
-
-            if left == 1:
-                if right == 0:
-                    new_points.append(self.points[index])
-        return new_points
+            if left == 1 and right == 0:
+                if y2 > elev_water:
+                    right += 1
+                    x3 = (elev_water-y1) * (x2-x1) / (y2-y1) + x1
+                    points.append((x3, elev_water))
+                else:
+                    points.append(pt)
+        return points
 
     def flow_area(self, depth):
         """Get the cross sectional area of flow, given a depth from the lowest point.
 
         Args:
-            depth (float): Depth, in :math:`feet`
+            depth (float): Depth, in :math:`feet`.
 
         Returns:
-            float: Wet area, in :math:`feet`.
+            float: Wet area, in :math:`feet^2`.
 
         """
 
-        vertices = self.get_new_vertices(depth)
-        n = len(vertices)
+        vertices = self.flow_vertices(depth)
         area = 0.0
-        for i in range(n):
+        for i, v in enumerate(vertices):
+            n = len(vertices)
             j = (i + 1) % n
-            area += vertices[i][0] * vertices[j][1]
-            area -= vertices[j][0] * vertices[i][1]
-        area = abs(area) / 2.0
-        return area
+            x1, y1 = v
+            x2, y2 = vertices[j]
+            area += x1 * y2
+            area -= x2 * y1
+        return abs(area) / 2.0
 
     def wet_perimeter(self, depth):
         """Get the wet perimeter of flow, given a depth from the lowest point.
@@ -291,54 +279,20 @@ class Irregular(Section):
             float: Wet perimeter, in :math:`feet`.
 
         """
-        p = 0.0
-        new_points = self.get_new_vertices(depth)
-        n = len(new_points)
-        for i in range(n-1):
-            p1 = new_points[i]
-            p2 = new_points[i+1]
-            p += self.point_distance([p1, p2])
-
-        return p
-
-    def point_distance(self, two_points):
-        """Get the distance between two given points.
-
-        Args:
-            two_points (float): Depth, in :math:`feet`
-
-        Returns:
-            float: Distance between two points, in :math:`feet`.
-
-        """
-        p1 = two_points[0]
-        p2 = two_points[1]
-        x1, y1 = p1
-        x2, y2 = p2
-
-        dist = math.sqrt((y2-y1)**2 + (x2-x1)**2)
-
-        return dist
+        perimeter = 0.0
+        vertices = self.flow_vertices(depth)
+        for pt1, pt2 in zip(vertices, vertices[1:]):
+            x1, y1 = pt1
+            x2, y2 = pt2
+            perimeter += math.hypot(x2 - x1, y2 - y1)
+        return perimeter
 
     @property
-    def get_lowest_elev(self):
+    def elev_lowest(self):
         """Get the elevation of the lowest point of the cross section.
-
-        Args:
 
         Returns:
             float: Elevation, in :math:`feet`.
 
         """
-        elevs = []                  # List of elevations (ordinates)
-        lowest = 0                  # Initial value of lowest
-        for point in self.points:
-            elevs.append(point[1])  # Iterate through the points and collect the ordinates
-        for i in range(len(elevs)):  # Find the lowest in the list of ordinates
-            if i == len(elevs):
-                break
-            if elevs[i] < lowest:
-                lowest = elevs[i]
-            else:
-                lowest = lowest
-        return lowest
+        return min(pt[1] for pt in self.points)
--- tests/test_irregular_section.py
+++ tests/test_irregular_section.py
@@ -1,3 +1,5 @@
+
+
 import unittest
 from pyflo import sections
 
@@ -5,16 +7,65 @@ from pyflo import sections
 class IrregularSectionTest(unittest.TestCase):
     """
     From https://www.researchgate.net/post/How_can_get_the_wetted_perimeter_in_an_irregular_stream_bank
+
     """
 
     def test_problem(self):
         # Calculate the cross sectional area given
-        points = [(0, 0), (5, -2.1), (10, -3.4), (15, -5.6),
-                  (20, -4.7), (25, -3.5), (30, -4.4), (35, -5.4),
-                  (40, -6.1), (45, -5.8), (50, -5.7), (55, -5.1),
-                  (60, -6), (65, -6.5), (70, -7.2), (75, -7.2),
-                  (80, -8.2), (85, -5.5), (90, -3.6), (95, -3.2), (100, 0)]
+        points = [
+            (000.0, 00.0),
+            (005.0, -2.1),
+            (010.0, -3.4),
+            (015.0, -5.6),
+            (020.0, -4.7),
+            (025.0, -3.5),
+            (030.0, -4.4),
+            (035.0, -5.4),
+            (040.0, -6.1),
+            (045.0, -5.8),
+            (050.0, -5.7),
+            (055.0, -5.1),
+            (060.0, -6.0),
+            (065.0, -6.5),
+            (070.0, -7.2),
+            (075.0, -7.2),
+            (080.0, -8.2),
+            (085.0, -5.5),
+            (090.0, -3.6),
+            (095.0, -3.2),
+            (100.0, 00.0)
+        ]
         s = sections.Irregular(points)
         produced = s.flow_area(8.2)
         expected = 496
         self.assertAlmostEqual(produced, expected, 1)
+
+    # def test_problem_2(self):
+    #     # Calculate the cross sectional area given
+    #     points = [
+    #         (00.0, 00.0),
+    #         (05.0, -91.0),
+    #         (10.0, -19.0),
+    #         (15.0, -96.0),
+    #         (20.0, -76.0),
+    #         (25.0, -71.0),
+    #         (30.0, -70.0),
+    #         (35.0, -81.0),
+    #         (40.0, -24.0),
+    #         (45.0, -4.0),
+    #         (50.0, -41.0),
+    #         (55.0, -43.0),
+    #         (60.0, -15.0),
+    #         (65.0, -10.0),
+    #         (70.0, -78.0),
+    #         (75.0, -48.0),
+    #         (80.0, -51.0),
+    #         (85.0, -12.0),
+    #         (90.0, -68.0),
+    #         (95.0, -72.0),
+    #         (100.0, 00.0),
+    #     ]
+    #     s = sections.Irregular(points)
+    #     produced = s.flow_area(8.2)
+    #     expected = 4993.33
+    #     self.assertAlmostEqual(produced, expected, 2)
